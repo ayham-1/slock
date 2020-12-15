97e25ac2ee2179a2243c6f44778cea8c68f41324
altffour <altffour@protonmail.com>
2020-12-15 07:51:21 +0300

    apply patches

--- config.def.h
+++ config.def.h
@@ -10,3 +10,27 @@ static const char *colorname[NUMCOLS] = {
 
 /* treat a cleared input like a wrong password (color) */
 static const int failonclear = 1;
+
+/* default message */
+static const char * message = "Suckless: Software that sucks less.";
+
+/* text color */
+static const char * text_color = "#ffffff";
+
+/* text size (must be a valid size) */
+static const char * font_name = "6x13";
+
+/* time in seconds before the monitor shuts down */
+static const int monitortime = 5;
+
+/* allow control key to trigger fail on clear */
+static const int controlkeyclear = 0;
+
+/*Enable blur*/
+#define BLUR
+/*Set blur radius*/
+static const int blurRadius=5;
+/*Enable Pixelation*/
+//#define PIXELATION
+/*Set pixelation radius*/
+static const int pixelSize=0;
